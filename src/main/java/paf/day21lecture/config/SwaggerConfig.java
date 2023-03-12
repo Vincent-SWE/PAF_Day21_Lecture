501ea41434133f3952dc03dcfa0bac05ed35f059
Vincent <VincentNSWE@gmail.com>
2023-03-12 16:01:42 +0800

    small_updated

--- src/main/java/paf/day21lecture/config/SwaggerConfig.java
+++ src/main/java/paf/day21lecture/config/SwaggerConfig.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.models.info.Info;
 @Configuration
 public class SwaggerConfig {
 
+    // Swagger is not an exam item
     @Bean
     public OpenAPI openApi() {
         return new OpenAPI()
